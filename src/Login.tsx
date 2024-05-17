7222ffc1f4eaa42fa5838d5e70f6b22c583a10a1
yamkin_a <me@jamkin.ru>
2024-05-17 14:30:23 +0500

    Deleted some components and added nmp material.

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
+import { Container, Typography, TextField, Button } from '@mui/material';
+import './App.css';
 
 function Login() {
     return (
-        <div className="login-page">
-            <h2 className="login-title">Вход в систему</h2>
-            <form className="login-form">
-                <div className="form-group">
-                    <label htmlFor="username">Имя пользователя:</label>
-                    <input type="text" id="username" name="username" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Пароль:</label>
-                    <input type="password" id="password" name="password" />
-                </div>
-                <button type="submit">Войти</button>
+        <Container maxWidth="xs" className="custom-container">
+            <Typography variant="h4" gutterBottom>
+                Вход в систему
+            </Typography>
+            <form noValidate autoComplete="off">
+                <TextField
+                    id="username"
+                    name="username"
+                    label="Имя пользователя"
+                    fullWidth
+                    margin="normal"
+                    className="custom-textfield"
+                />
+                <TextField
+                    id="password"
+                    name="password"
+                    label="Пароль"
+                    type="password"
+                    fullWidth
+                    margin="normal"
+                />
+                <Button variant="contained" fullWidth>
+                    Войти
+                </Button>
             </form>
-        </div>
+        </Container>
     );
 }
 
