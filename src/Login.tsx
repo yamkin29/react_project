76c2d3852fe9ff7430d19aca392b4ae54ceb39ff
yamkin_a <me@jamkin.ru>
2024-05-20 15:03:35 +0500

    Added some minor edits.

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import { Container, Typography, TextField, Button } from '@mui/material';
 
-function Login() {
-    return (
-        <Container maxWidth="xs" className="custom-container">
-            <Typography variant="h4" gutterBottom>
-                Вход в систему
-            </Typography>
-            <form noValidate autoComplete="off">
-                <TextField
-                    id="username"
-                    name="username"
-                    label="Имя пользователя"
-                    fullWidth
-                    margin="normal"
-                />
-                <TextField
-                    id="password"
-                    name="password"
-                    label="Пароль"
-                    type="password"
-                    fullWidth
-                    margin="normal"
-                />
-                <Button variant="contained" fullWidth>
-                    Войти
-                </Button>
-            </form>
-        </Container>
-    );
+class Login extends React.Component {
+    constructor({props}: { props: any }) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+    render() {
+        return (
+            <Container maxWidth="xs" className="custom-container">
+                <Typography variant="h4" gutterBottom>
+                    Вход в систему
+                </Typography>
+                <form noValidate autoComplete="off">
+                    <TextField
+                        id="username"
+                        name="username"
+                        label="Имя пользователя"
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        id="password"
+                        name="password"
+                        label="Пароль"
+                        type="password"
+                        fullWidth
+                        margin="normal"
+                    />
+                    <Button variant="contained" fullWidth>
+                        Войти
+                    </Button>
+                </form>
+            </Container>
+        );
+    }
 }
 
 export default Login;
